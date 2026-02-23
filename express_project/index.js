bcc2bab21e0120effa3836095c1841d0e45ce48e
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-02-23 19:19:37 +0600

    api create and test

--- express_project/index.js
+++ express_project/index.js
@@ -1,6 +1,7 @@
 let express=require('express');
 
 let app=express();
+app.use(express.json()); //for parsing application/json
 
 app.get('/',(req, res)=>{
     res.send('Home Page API');
@@ -13,6 +14,42 @@ app.get('/news',(req, res)=>{
             msg:"News API"
         }
     );
+})
+
+app.get('/products',(req, res)=>{
+    res.send(
+        {
+            status:1, 
+            msg:"Products API"
+        }
+    );
+})
+
+
+app.post('/login',(req, res)=>{
+    console.log(req.body);  // for debugging purpose
+
+    res.status(200).send(
+        {
+            status:1,
+            msg:"Login API",
+            bodyData:req.body,
+            queryData:req.query
+        }
+    );
+    // res.send(
+    //     {
+    //         status:1, 
+    //         msg:"Login API",
+    //         bodyData:req.body,
+    //         queryData:req.query
+    //     }
+    // );
+})
+
+app.get('/news/:id',(req, res)=>{
+    let currentNewsId=req.params.id;
+    res.send("News Details API"+currentNewsId);
 })
 
 
